a620614c87f6b969c3c9948ae5563c98e816d597
Phil Pursglove <philpursglove@users.noreply.github.com>
2020-07-29 15:16:43 +0100

    NameChecker created and first check for number of elements

---
+++ XanthosCodeTest/XanthosCodeTest.Tests/NameCheckerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace XanthosCodeTest.Tests
+{
+    [TestFixture]
+    public class NameCheckerTests
+    {
+        [TestCase("Jules Gabriel Verne", true)]
+        [TestCase("J G Verne", true)]
+        [TestCase("Jules Verne", true)]
+        [TestCase("Jules", false)]
+        [TestCase("M. Jules Gabriel Verne", false)]
+        public void Name_Must_Contain_Two_Or_Three_Elements(string name, bool expected)
+        {
+            NameChecker checker = new NameChecker();
+            bool result = checker.CheckNumberOfElementsInName(name);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ XanthosCodeTest/XanthosCodeTest/NameChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XanthosCodeTest
+{
+    public class NameChecker
+    {
+        public IEnumerable<string> Elements { get; set; }
+
+        public bool CheckNumberOfElementsInName(string name)
+        {
+            SplitNameIntoElements(name);
+
+            return Elements.Count() == 2 || Elements.Count() == 3;
+        }
+
+        private IEnumerable<string> SplitNameIntoElements(string name)
+        {
+            Elements = name.Split(' ');
+        }
+    }
+}
